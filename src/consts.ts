0cd5ed4b615da126d3c912ab362b0da3a7275387
Leonhard Melzer <leonhard.melzer@peerigon.com>
2024-10-21 16:09:06 +0200

    feat: light theme customization

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Write & Hike";
+export const SITE_DESCRIPTION =
+  "Personal blog about my experience writing software and occasional hiking.";
